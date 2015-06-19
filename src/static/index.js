9103a5ccd8dd73f113c62aaba04b236599fdffc4
Holi0317 <holliswuhollis@gmail.com>
2015-06-19 19:58:16 +0800

    Fix clicking activated sub-menu will hide context

--- src/static/index.js
+++ src/static/index.js
@@ -9,8 +9,13 @@ $(function (){
     var item = $(subMenuChild[i]);
     item.on('click', function( event ) {
       event.preventDefault();
+      if ( $(this).hasClass('active') ) {
+        // already actived
+        return;
+      }
       // Remove old context
-      var selected = $('#sub-nav').children('.active').toggleClass('active');
+      var selected = $('#sub-nav').children('.active');
+      selected.toggleClass('active');
       $(selected.data().boundTo).collapse('toggle');
 
       // Show selected context
