7f6743dce091295718af2b0a8c6793abbd7d3841
holi0317 <holliswuhollis@gmail.com>
2015-06-20 21:20:35 +0800

    Responsive design for navbar, and hide some content for mobile view

--- src/static/js/onload.js
+++ src/static/js/onload.js
@@ -4,4 +4,4 @@ $(function() {
   $("ul.nav a[href='" + url + "']").parent().addClass('active');
   $("ul.nav a[href='" + url.substring(1) + "']").parent().addClass('active');
   $('ul.nav a').filter(function() {return this.href == url;}).parent().addClass('active');
-})
+});
