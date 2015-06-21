46893747bcfa0369f096adbeb2eab9cc5f691ec7
Holi0317 <holliswuhollis@gmail.com>
2015-06-21 19:27:40 +0800

    Add close button for alerts

--- src/static/js/_onload.js
+++ src/static/js/_onload.js
@@ -14,14 +14,22 @@ $(function() {
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+
+  // When close button is clicked, hide its parent container
+  $('button.close').on('click', function ( event ) {
+    event.preventDefault();
+    $(this).parents('.container').first().addClass('hidden');
+  });
 });
 
 // View switch
 $(function() {
   // As saving of settings requires webstorage api,
   // if browser does not support them, nothing will show on the screen
-  
+
   var screenWidth = $(window).width();
+  var allViewSwitch;
+  var viewSwitch;
 
   if ( typeof(Storage) !== "undefined") {
     // Vertify support of Storage
@@ -35,17 +43,23 @@ $(function() {
 
       console.log('forceDesktop: ' + localStorage.forceDesktop);
 
-      // switch viewport and show toggle button
-      var viewport = $("head meta[name='viewport']");
-      var viewSwitch = $('#view-switch').removeClass('hidden');
+      allViewSwitch = $('#view-switch').removeClass('hidden');
       if ( localStorage.forceDesktop === "true" ) {
-        viewport.attr('content', 'width=1024, initial-scale=1');
-        viewSwitch.children("[data-target='mobile']").removeClass('hidden').children('button').on('click', function() {
+        $("meta[name='viewport']").attr('content', 'width=1024, initial-scale=1');
+        // Show toggle box
+        viewSwitch = allViewSwitch.children("[data-target='mobile']").removeClass('hidden');
+        
+        // register event
+        viewSwitch.children('.toggle-button').on('click', function() {
           localStorage.forceDesktop = false;
           location.reload(true);
         });
       } else {
-        viewSwitch.children("[data-target='desktop']").removeClass('hidden').children('button').on('click', function() {
+        // Show toggle box
+        viewSwitch = allViewSwitch.children("[data-target='desktop']").removeClass('hidden');
+        
+        // register event
+        viewSwitch.children('.toggle-button').on('click', function() {
           localStorage.forceDesktop = true;
           location.reload(true);
         });
